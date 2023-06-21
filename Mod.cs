263933b6d3ee479c244563b3144a0f70b80b2398
adon <86583577+shg166@users.noreply.github.com>
2023-06-21 10:36:21 +0900

    Add Code & Name List

---
+++ Mod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.UI;
+using ICities;
+
+namespace JP_PedstrianNameList
+{
+	public class JP_PedstrianNameList : IUserMod
+	{
+
+		public string Name
+		{
+			get { return "JP Pedstrian Name List (CNL) v1.0"; }
+		}
+
+		public string Description
+		{
+			get { return "Custom Name Listsを使って、DLC Plazas & Promenadesの歩行者エリア名を変更します"; }
+		}
+
+	}
+}
